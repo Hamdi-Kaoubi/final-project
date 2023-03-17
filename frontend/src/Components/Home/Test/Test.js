4cc9dcb8ad39d740dcc05197f078ac770bd4dbb6
Hamdi-Kaoubi <hamdi.kaoubi1994@gmail.com>
2023-03-17 12:44:08 +0100

    second commit

--- frontend/src/Components/Home/Test/Test.js
+++ frontend/src/Components/Home/Test/Test.js
@@ -1,30 +1,31 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { get_languages } from '../../../redux/action/Action'
-import './Test.css'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Spinner from 'react-bootstrap/Spinner';
+//import './Test.css'
 const Test = () => {
-    const dispatch=useDispatch()
-    useEffect(() => {
-      dispatch(get_languages())
-    }, [dispatch])
-    const programmings=useSelector((state)=>state.ProgramReducer.programmings)
+  const [data,setData]=useState([])
+  const [loaded,setLoaded] = useState(true)
+  useEffect(() => {
+    axios.get('/getlanguage').then((res)=>setData(res.data))
+    .then(()=>setLoaded(false))
+  }, [])
   return (
-    <div>
-    {programmings[0].content.map((elm)=>
+    <div className='videos-container'>
+      {loaded==true?<div><Spinner className='spn' animation="border" role="status">
+      </Spinner></div>:data.map((el)=>el.content.map((elm)=>
         <div className="video-card">
-    <header>
-      <a href="#"><img src={elm.imgUrl} /></a>
-      <h1>{elm.vidTitle}</h1>
-      <h2>{elm.creator}</h2>
-    </header>
-    <div className="video-bio">
-      <p>{elm.date}</p>
-      <button>Wishlist</button>
-      <button className="cart">Add to cart</button>
-    </div>
-  </div>
-        
-        )}
+        <header>
+          <img src={elm.imgUrl} alt=''/>
+          <h1>{elm.vidTitle}</h1>
+          <h2>{elm.creator}</h2>
+        </header>
+        <div className="video-bio">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis ligula vitae lorem tempus maximus.</p>
+          <button>Wishlist</button>
+          <button className="cart">Add to cart</button>
+        </div>
+      </div>
+      ))}
     </div>
   )
 }
